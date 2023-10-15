733280e1935c418979170ef50acdb5f0079f8fa6
Roopa <roopa.usa.cm@gmail.com>
2023-10-14 20:32:48 -0500

    updates with command line commands.

--- InsuranceProject/config.py
+++ InsuranceProject/config.py
@@ -13,3 +13,8 @@ UPLOAD_FOLDER = os.path.join('my_sql_temp_data')
 DATA_FOLDER = os.path.join('my_sql_temp_user_info', 'data.csv')
 # Define allowed files
 ALLOWED_EXTENSIONS = {'csv'}
+
+
+# flask --app app  run --debug  
+# py -m venv env      -- To create vitrual environment
+# env\Scripts\activate   -- To activate the virtual environment
